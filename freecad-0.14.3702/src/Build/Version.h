cbcc047d5af4a06da3a77b32eabb68761ee3a4e4
AlexandreRivet <alex-rivet94@hotmail.fr>
2015-01-10 14:12:10 +0100

    Add new files

---
+++ freecad-0.14.3702/src/Build/Version.h
@@ -0,0 +1,14 @@
+
+// Version Number
+#define FCVersionMajor "0"
+#define FCVersionMinor "14"
+#define FCVersionName  "Vulcan"
+// test: $Format:Hash (%H), Date: %ci$
+#define FCRevision      "3702 (Git)"      //Highest committed revision number
+#define FCRevisionDate  "2014/07/13 12:32:32"     //Date of highest committed revision
+#define FCRepositoryURL "ssh://yorikvanhavre@git.code.sf.net/p/free-cad/code"      //Repository URL of the working copy
+
+// Git relevant stuff
+#define FCRepositoryHash   "b3368125c63289ec8ce9faec2b2ae4c78d436406"
+#define FCRepositoryBranch "releases/FreeCAD-0-14"
+
